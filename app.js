2e7892aa4a9096fd85aacfa644905dece5816072
Jonathas <contato@jonathas.com>
2016-07-02 16:20:48 +0200

    Transformed it into a class

--- app.js
+++ app.js
@@ -1,57 +1,64 @@
 // This works now only for the photos opened from the profile page
+"use strict";
 
-var mediaLinksList = document.getElementsByClassName('_8mlbc');
 var buttonImage = "https://cdn2.iconfinder.com/data/icons/freecns-cumulus/16/519672-178_Download-128.png";
+var context = "";
 
-function init() {
-    setDownloadLink();
+class ProfilePageDownloader {
 
-    //Attach listeners
-    document.getElementsByClassName("coreSpriteLeftPaginationArrow")[0].addEventListener("click",
-        function(e) {
-            setDownloadLink(e.target);
+    constructor() {
+        this.mediaLinksList = document.getElementsByClassName('_8mlbc');
+
+        this.setDownloadLink();
+        context = this;
+
+        //Attach listeners
+        document.getElementsByClassName("coreSpriteLeftPaginationArrow")[0].addEventListener("click",
+            function(e) {
+                context.setDownloadLink(e.target);
+            }
+        );
+        document.getElementsByClassName("coreSpriteRightPaginationArrow")[0].addEventListener("click",
+            function(e) {
+                context.setDownloadLink(e.target);
+            }
+        );
+    }
+
+    setDownloadLink(e) {
+        var mediaCodeUrl = this.setMediaCodeUrl(e);
+        var mediaCode = "";
+        var mediaLink = "";
+
+        for(var i = 0; i < this.mediaLinksList.length;i++) {
+            mediaCode = this.mediaLinksList[i].getAttribute("href").split("/")[2];
+
+            if(mediaCode == mediaCodeUrl) {
+                mediaLink = this.mediaLinksList[i].getElementsByClassName('_icyx7')[0].getAttribute('src');
+                break;
+            }
         }
-    );
-    document.getElementsByClassName("coreSpriteRightPaginationArrow")[0].addEventListener("click",
-        function(e) {
-            setDownloadLink(e.target);
-        }
-    );
-};
 
-function setDownloadLink(e) {
-    var mediaCodeUrl = setMediaCodeUrl(e);
-    var mediaCode = "";
-    var mediaLink = "";
+        this.setDownloadButton(mediaLink);
+    }
 
-    for(i = 0; i < mediaLinksList.length;i++) {
-        mediaCode = mediaLinksList[i].getAttribute("href").split("/")[2];
-
-        if(mediaCode == mediaCodeUrl) {
-            mediaLink = mediaLinksList[i].getElementsByClassName('_icyx7')[0].getAttribute('src');
-            break;
+    setMediaCodeUrl(e) {
+        //Had to get the link from the arrow because the new download link was being set before the URL would change
+        if(e == undefined) {
+            return window.location.href.toString().split(window.location.host)[1].split("/")[2];
+        } else {
+            //Here we set the right download link if it's not coming from a click event (first time)
+            return e.getAttribute('href').split("/")[2];
         }
     }
 
-    setDownloadButton(mediaLink);
-};
-
-function setMediaCodeUrl(e) {
-    //Had to get the link from the arrow because the new download link was being set before the URL would change
-    if(e == undefined) {
-        return window.location.href.toString().split(window.location.host)[1].split("/")[2];
-    } else {
-        //Here we set the right download link if it's not coming from a click event (first time)
-        return e.getAttribute('href').split("/")[2];
+    setDownloadButton(mediaLink) {
+        if (document.getElementsByClassName('download-media').length > 0) {
+          document.getElementsByClassName('download-media')[0].href = mediaLink;
+        } else {
+            document.getElementsByClassName('_s6yvg')[0].innerHTML = document.getElementsByClassName('_s6yvg')[0].innerHTML + '&nbsp;&nbsp;<a class="download-media" href="' + mediaLink + '" download="instagram.jpg"><img src="' + buttonImage + '" width="20"></a>';
+        }
     }
-};
+}
 
-function setDownloadButton(mediaLink) {
-    if (document.getElementsByClassName('download-media').length > 0) {
-      document.getElementsByClassName('download-media')[0].href = mediaLink;
-    } else {
-        document.getElementsByClassName('_s6yvg')[0].innerHTML = document.getElementsByClassName('_s6yvg')[0].innerHTML + '&nbsp;&nbsp;<a class="download-media" href="' + mediaLink + '" download="instagram.jpg"><img src="' + buttonImage + '" width="20"></a>';
-    }
-};
-
-init();
+var profile = new ProfilePageDownloader();
